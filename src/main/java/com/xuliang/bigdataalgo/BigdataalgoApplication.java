3a7b8903c4dd42c7824d18b18bbcbf38b62e09c0
xuliang98 <_DaDiDa1995>
2021-01-28 23:48:42 +0800

    git init

---
+++ src/main/java/com/xuliang/bigdataalgo/BigdataalgoApplication.java
@@ -0,0 +1,13 @@
+package com.xuliang.bigdataalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdataalgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigdataalgoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuliang/bigdataalgo/BigdataalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuliang.bigdataalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdataalgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
